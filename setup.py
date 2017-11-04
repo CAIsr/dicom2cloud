4bd6ca912bb28c85db9c2796ab056e719e614274
Isaac Lenton <isaac@isuniversal.com>
2017-11-04 21:08:34 +1000

    Corrected license tag in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'Topic :: Scientific/Engineering :: Medical Science Apps.',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD 2-Clause License',
+        'License :: OSI Approved :: BSD License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
