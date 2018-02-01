7b6ee755a74ff1bffeb0d8ab3924d625a482ebe4
Isaac Lenton <isaac@isuniversal.com>
2018-02-01 18:05:46 +1000

    Interface version check implemented

--- js/updateCheck.js
+++ js/updateCheck.js
@@ -3,7 +3,7 @@
  */
 
 var divOnline = document.getElementById("interface_online");
-var divLatest = document.getElementById("interface_latest");
+var divLatest = document.getElementById("interface_ok");
 var divUpdate = document.getElementById("interface_update");
 var divConnect = document.getElementById("interface_connect");
 
@@ -15,9 +15,26 @@ divConnect.style.display = "block";
 // Set the current version
 $("#interface\\_current").text("Current version: " + thisVersion);
 
-// Set the latest version
-fetch('https://caisr.github.io/dicom2cloud/uiversion.js')
-  .then(response => response.text())
-  .then(text => $("#interface\\_latest").html(text));
+if (window.location.hostname == "caisr.github.io") {
+  divOnline.style.display = "block";
+  divConnect.style.display = "none";
+} else {
 
+  // Set the latest version
+  fetch('https://caisr.github.io/dicom2cloud/uiversion.js')
+    .then(response => response.text())
+    .then(function(text) {
+      console.log(typeof text);
+      var version = text.split("=")[1].trim().slice(1, -2);
+      $("#interface\\_latest").text("Latest version: " + version);
+
+      if (version == thisVersion) {
+        divLatest.style.display = "block";
+        divConnect.style.display = "none";
+      } else if (version != thisVersion) {
+        divUpdate.style.display = "block";
+        divConnect.style.display = "none";
+      }
+    });
+}
 
